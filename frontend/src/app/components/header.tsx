b524150d8f3de8ec37e6604f48c1859703d5082e
shiratori <shiratori@elplanning.co.jp>
2025-09-05 15:59:10 +0900

    🎉

---
+++ frontend/src/app/components/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return <div className="">
+    <h1 className="text-3xl font-bold">Next.js × Hono Todo</h1>
+  </div>;
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div>
+        <h1 className="">Hello, Hono!</h1>
+      </div>
+    </>
+  );
+}
